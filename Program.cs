43644055e0d4a208b11df9944b3b10a23e2e7af6
tranzert <gonzalo.tonanes@gmail.com>
2020-05-22 19:53:39 -0300

    primer commit del proyecto

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: gonza
+ * Date: 22/5/2020
+ * Time: 18:13
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace TPF_CTEDyA
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
